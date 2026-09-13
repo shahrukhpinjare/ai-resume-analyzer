b5abc93e4a7c1ad34bcd6f5223ca6bfe8cbeac46
Shahrukhraza09 <148698182+Shahrukhraza09@users.noreply.github.com>
2026-09-13 16:02:22 +0530

    Fix cross-origin cookie setting with samSite none and secure

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -40,7 +40,12 @@ async function registerUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 day
+  });
 
   res.status(201).json({
     message: "USer registered successfully",
